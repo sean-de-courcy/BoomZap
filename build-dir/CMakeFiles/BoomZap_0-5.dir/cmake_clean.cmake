55318fc697368dd7ad0033be993e0fd5df8cfe13
Sean Coursey <seanecoursey@gmail.com>
2021-07-05 17:04:37 -0700

    Removed build-dir from .gitignore because of the necessary font file.

---
+++ build-dir/CMakeFiles/BoomZap_0-5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BoomZap_0-5"
+  "BoomZap_0-5.pdb"
+  "CMakeFiles/BoomZap_0-5.dir/source/BoomZap.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BoomZap_0-5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
